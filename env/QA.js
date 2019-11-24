375d2547e7095505fd088b08f5f89481aa8b7fc4
Elavarasan Saravanan <e10909elavarasanexcelity@gmail.com>
2019-11-24 10:36:53 +0530

    ERP application for registration and login

--- env/QA.js
+++ env/QA.js
@@ -1,3 +1,17 @@
 module.exports = {
-
+    serverPort: 8012,
+    clientdb: 'mongodb://127.0.0.1:27017/',
+    dbname:"ERP",
+    cryptoToken:"ERPCRYPTOTOKENFORLOGIN",
+    jwtToken:"ERPWEBTOKEN_TOVALIDATEREQUEST",
+    sessionexpiry: "15m",
+    mail: {
+         //Add the email configrations here
+         service: 'gmail',
+         secure: false, 
+            auth: {
+    user: 'erpmarketting@gmail.com',
+    pass: 'erp@1234'
+  }
+}
 };
